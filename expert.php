6f9cc0795b10cb3b9748840b352e7267c34e1da3
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-10-02 12:20:17 +0200

    ex 4

--- expert.php
+++ expert.php
@@ -35,6 +35,16 @@ echo substr($str, 0, 16);
 
 //added some points, space and single quotes
 
+// === Exercise 4 ===
+// Sometimes debugging code is just like looking up code and syntax...
+// The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
+// Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
+
+foreach($week as &$day) {
+    $day = substr($day, 0, strlen($day)-3);
+}
+
+print_r($week);
 
 
 
